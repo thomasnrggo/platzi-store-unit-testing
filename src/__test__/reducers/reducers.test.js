1a8cc64bdadbcc96c2faaa2caf8d2baf6a4437ee
Anthony Gonzalez <thomasnrggo@gmail.com>
2021-05-07 17:21:41 -0500

    testing reducers

---
+++ src/__test__/reducers/reducers.test.js
@@ -0,0 +1,26 @@
+import reducer from '../../reducers'
+import ProductMock from '../../__mocks__/productMock'
+
+describe('Reducers', () => {
+    test('return initial state', () => {
+        expect(reducer({}, '')).toEqual({})
+    })
+    test('ADD_TO_CART', () => {
+        const initialState = {
+            cart: []
+        }
+        const payload = ProductMock
+        const action = {
+            type: 'ADD_TO_CART',
+            payload
+        }
+        const expected = {
+            cart:[
+                ProductMock
+            ]
+        }
+        expect(reducer(initialState, action)).toEqual(expected)
+    })
+    
+    
+});
